516a8d751a4f0ec56d59831ab44add74878e00b2
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2024-03-06 17:39:21 +0530

    Update ecr.py

--- ecr.py
+++ ecr.py
@@ -1,9 +1,17 @@
 import boto3
 
-ecr_client = boto3.client('ecr')
+aws_access_key_id = 'AKIAZQ3DQ4GWRVR332OF'
+aws_secret_access_key = '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'
+aws_region = 'us-east-1'
 
-repository_name = "my_monitoring_app_image"
+ecr_client = boto3.client('ecr',
+                          aws_access_key_id=aws_access_key_id,
+                          aws_secret_access_key=aws_secret_access_key,
+                          region_name=aws_region)
+
+repository_name = "trial_and_tested"
+
 response = ecr_client.create_repository(repositoryName=repository_name)
 
-repository_uri = response ['repository']['repositoryUri']
+repository_uri = response['repository']['repositoryUri']
 print(repository_uri)
